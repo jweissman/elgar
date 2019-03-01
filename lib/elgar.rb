a5825cb5606e4384d8d5a7c9021a58e511d2350a
Joseph Weissman <jweissman@pivotal.io>
2019-03-01 17:30:40 -0500

    sketch lex/parse

--- lib/elgar.rb
+++ lib/elgar.rb
@@ -1,6 +1,84 @@
 require 'elgar/version'
 
 module Elgar
+  module Tokens
+    class Num < Struct.new(:value); end
+    class Op < Struct.new(:value); end
+    class Id < Struct.new(:value); end
+  end
+
+  class Lexer
+    def tokenize(str)
+      tokens = []
+      # consume tokens...
+      scanner = StringScanner.new(str)
+      until scanner.eos?
+        # try to parse!!!
+        if res = scanner.scan(/\d+/)
+          tokens.push(Num[res.to_i])
+        elsif res = scanner.scan(/\w+/)
+          tokens.push(Id[res])
+        elsif res = scanner.scan(/[+]/)
+          tokens.push(Op[res.to_sym])
+        else
+          raise "Tokenization error in string #{str}: unrecognized character... (at pos=#{scanner.pos})"
+        end
+      end
+      #
+      tokens
+    end
+  end
+
+  module ASTNodes
+    class Int < Struct.new(:value); end
+
+    class Add < Struct.new(:left, :right)
+    end
+  end
+
+  class Parser
+    # [ '=', '1', '+', '2' ] => ['+', [ 1, 2 ]]
+    def tree(str)
+      tokens = Lexer.new.tokenize(str)
+      expr(tokens)
+    end
+
+    ### expr
+
+    # def expr
+    #   # do we have val op expr??
+    #   # match(...) / consume(...)
+    #   # Add[1,2]
+    #   [ val, op, expr ]
+    # end
+
+    # def val #(tkns)
+    #   num #(tkns)
+    #   # if tkns.
+    # end
+
+    # # just give back Int?
+    # def num(tkns)
+    #   tkns.first.is_a?(Num) && Int[tkns.pop.value]
+    # end
+
+    # todo -- test and implement these???
+    def peek; end
+    def consume; end
+  end
+
+  class Formula
+    def self.from_expression(input)
+      ast = parse(input)
+      self.new(ast)
+    end
+
+    # give back AST...
+    def self.parse(input)
+      Parser.new.tree(input)
+    end
+  end
+
   class Sheet < Struct.new(:name)
     def write(info, at:)
       puts "---> Sheet #{@name}: write #{info} to cell #{at}"
@@ -10,6 +88,11 @@ module Elgar
     def read(address)
       value = database[address]
       puts "---> Sheet #{@name}: read from cell at #{address}"
+      if value.start_with?('=')
+        # raise "Would compute formula: #{value}"
+        compute_formula(value)
+      end
+      return value
     end
 
   private
@@ -18,5 +101,9 @@ module Elgar
       @store ||= {}
     end
 
+    def compute_formula(value)
+      # parse and compute...
+      Formula.from_expression(value).compute(self)
+    end
   end
 end
--- spec/elgar_spec.rb
+++ spec/elgar_spec.rb
@@ -1,7 +1,37 @@
 require 'spec_helper'
 require 'elgar'
 
+include Elgar::Tokens
+include Elgar::ASTNodes
+
 describe Elgar do
+  describe Lexer do
+    it 'tokenizes' do
+      lex = Lexer.new()
+      expect(lex.tokenize('1+a2')).to eq([
+        Num[1],
+        Op[:+],
+        Id['a2']
+      ])
+    end
+  end
+
+  describe Parser do
+    xit 'assembles ast' do
+      parser = Parser.new
+      expect(parser.tree('1+2')).to eq(Add[1,2])
+      expect(parser.tree('1+2+3')).to eq(Add[Add[1,2],3])
+    end
+  end
+
+  xdescribe Formula do
+    it 'computes simple arith' do
+      fake_sheet = double(Sheet)
+      fx = Formula.from_expression('=1+2')
+      expect(fx.compute(fake_sheet)).to eq('3')
+    end
+  end
+
   describe Sheet do
     it 'has a name' do
       sheet = Sheet.new('cats')
@@ -12,6 +42,23 @@ describe Elgar do
       sheet = Sheet.new('felis cattus')
       sheet.write('hello', at: 'A1')
       expect(sheet.read('A1')).to eq('hello')
+    end
+
+    xit 'computes a formula' do
+      sheet = Sheet.new('felis cattus')
+      sheet.write('name', at: 'A1')
+      sheet.write('age', at: 'A2')
+
+      sheet.write('alison', at: 'B1')
+      sheet.write('1', at: 'B2')
+
+      sheet.write('ari', at: 'C1')
+      sheet.write('2', at: 'C2')
+
+      sheet.write('total', at: 'F1')
+      sheet.write('=sum(B2:E2)', at: 'F2')
+
+      expect(sheet.read('F2')).to eq('3')
     end
   end
 end
